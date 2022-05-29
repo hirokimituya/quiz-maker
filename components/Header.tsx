8ba03896fd5c9511806183588704ecfc9c68c59a
hirokimituya <hiromitu819@yahoo.co.jp>
2022-05-29 09:01:33 +0900

    add ダークモード機能の追加(localStorageを使用して半永続的に設定できる)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,11 @@
 import React from "react"
-import { AppBar, Box, Toolbar, Typography, Slide, useScrollTrigger } from "@mui/material"
+import { AppBar, Box, Toolbar, Typography, Slide, useScrollTrigger, IconButton, useTheme } from "@mui/material"
 import Link from "next/link"
 import Image from "next/image"
 import TextWhiteButton from "./common/TextWhiteButton"
+import { ColorModeContext } from "@pages/_app"
+import Brightness4Icon from "@mui/icons-material/Brightness4"
+import Brightness7Icon from "@mui/icons-material/Brightness7"
 
 type HideOnScrollProps = {
   children: React.ReactElement
@@ -18,12 +21,16 @@ function HideOnScroll({ children }: HideOnScrollProps) {
 }
 
 const Header = () => {
+  // ダークモード用のコンテキスト値
+  const theme = useTheme()
+  const colorMode = React.useContext(ColorModeContext)
+
   return (
     <header>
       <Box sx={{ flexGrow: 1 }}>
         <div id="back-to-top-anchor" />
         <HideOnScroll>
-          <AppBar>
+          <AppBar enableColorOnDark>
             <Toolbar>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 <Link href="/">
@@ -32,6 +39,14 @@ const Header = () => {
                   </a>
                 </Link>
               </Typography>
+              {/* ダークモード用のアイコン・ボタン */}
+              <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode}>
+                {theme.palette.mode === "dark" ? (
+                  <Brightness7Icon />
+                ) : (
+                  <Brightness4Icon sx={{ color: "common.white" }} />
+                )}
+              </IconButton>
               <TextWhiteButton size="large">ユーザー登録</TextWhiteButton>
               <TextWhiteButton size="large">ログイン</TextWhiteButton>
             </Toolbar>
