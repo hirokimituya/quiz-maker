b90c0894999618ea7c737fe62a992c1d94719189
hirokimituya <hiromitu819@yahoo.co.jp>
2022-06-04 09:37:29 +0900

    add Prismaのログ設定

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -13,10 +13,14 @@ declare global {
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
+  prisma = new PrismaClient({
+    log: ["query", "info", "warn", "error"]
+  })
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient()
+    global.prisma = new PrismaClient({
+      log: ["query", "info", "warn", "error"]
+    })
   }
   prisma = global.prisma
 }
